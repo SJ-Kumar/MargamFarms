dd5fe43703ecff2109eb836d4bc74c6eef7296c8
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-11 17:52:45 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+
 import Footer from './components/Footer';
 
 const App = () => {
