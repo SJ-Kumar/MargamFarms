bf0440a2b61a8cd11ad629562be676d351af9e05
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-11 23:09:01 +0530

    comitted

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -22,9 +23,6 @@ app.use(express.urlencoded({extended : true}));
 //Cookie Parser Middleware
 app.use(cookieParser());
 
-app.get('/',(req,res) => { 
-    res.send("API Running Bhaiya..");
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -34,7 +32,21 @@ app.use('/api/stripe', stripe);
 app.use(notFound);
 app.use(errorHandler);
 
-
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
