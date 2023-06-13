f534a22801836fd97911fd743f8688d490ff4544
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-13 18:23:21 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,21 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'
 import stripe from './routes/stripe.js';
-
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app=express();
+app.use(cors(
+{
+      origin: [""],
+      methods: ["POST", "GET"],
+      credentials: true
+}
+));
 
 app.get('/', (req, res) => {
   res.send('API is running....');
@@ -32,12 +40,10 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 app.use('/api/stripe', stripe);
 
-app.use(notFound);
-app.use(errorHandler);
 
-/*
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
     app.use('/uploads', express.static('/var/data/uploads'));
@@ -53,7 +59,12 @@ if (process.env.NODE_ENV === 'production') {
       res.send('API is running....');
     });
 }
-*/
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
 
