4b7c08c05c80991d99e545d8a989535b94ddc13f
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-13 18:42:47 +0530

    try

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js'
 import stripe from './routes/stripe.js';
 import cors from 'cors';
-import mongoose from 'mongoose';
 
 const port = process.env.PORT || 5000;
 
@@ -31,11 +29,7 @@ app.use(express.urlencoded({extended : true}));
 //Cookie Parser Middleware
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://MargamFarms:MargamSuresh@cluster0.6xchar7.mongodb.net/MargamFarms?retryWrites=true&w=majority');
 
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
 
 
 
@@ -44,7 +38,6 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
 app.use('/api/stripe', stripe);
 
 
