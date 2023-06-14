a5e9ea0d6810fae0b06f012e164e3626c329cfb2
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-14 08:34:10 +0530

    fixed whatsapp number bug

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
               <SocialIcon url='https://twitter.com/MargamFarm' target='_blank' rel='noopener noreferrer' className="social-icon" />
               <SocialIcon url='https://www.youtube.com/@margamfarm638' target='_blank' rel='noopener noreferrer' className="social-icon" />
               <SocialIcon url='mailto:margamfarm@gmail.com' target='_blank' rel='noopener noreferrer' className="social-icon" network="email" />
-              <SocialIcon url='https://wa.me/8884345668' target='_blank' rel='noopener noreferrer' className="social-icon" network="whatsapp" />
+              <SocialIcon url='https://wa.me/+918884345668' target='_blank' rel='noopener noreferrer' className="social-icon" network="whatsapp" />
               <SocialIcon url='https://margamfarm.medium.com/' target='_blank' rel='noopener noreferrer' className="social-icon" network="medium" />
             </div>
           </Col>
