17df3e9957236c41990015a9783e23b1ec7f1e72
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-07-03 22:04:28 +0530

    carousel updates

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
 import Message from './Message';
 import { useGetTopProductsQuery } from '../slices/productsApiSlice';
+import blurredBackgroundImage from './trying.png';
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
@@ -9,17 +10,18 @@ const ProductCarousel = () => {
   return isLoading ? null : error ? (
     <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-primary mb-4'>
+    <Carousel pause='hover' className='mb-4'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <div className='carousel-image-container'>
               <Image src={product.image} alt={product.name} fluid className='carousel-image round-image' />
+              <div className='blurred-background-image' style={{ backgroundImage: `url(${blurredBackgroundImage})` }} />
               <Carousel.Caption className='carousel-caption'>
-              <h2 className='text-white text-right'>
-                {product.name}
-              </h2>
-            </Carousel.Caption>
+                <h2 className='text-white text-right'>
+                  {product.name}
+                </h2>
+              </Carousel.Caption>
             </div>
           </Link>
         </Carousel.Item>
