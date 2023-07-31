b87060a71bebc5da1a7358a248ccaac369d2668c
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-07-31 17:48:38 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 import Footer from './components/Footer';
-
 const App = () => {
   return (
     <>
