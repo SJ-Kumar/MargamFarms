c16d274c7ad755c75ef524839323f28cbc055dbd
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-09-12 20:09:25 +0530

    chnages

--- backend/server.js
+++ backend/server.js
@@ -10,46 +10,46 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import cors from 'cors';
-
+import { fileURLToPath } from 'url';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app=express();
-app.use(cors(
+/* app.use(cors(
 {
       origin: ["https://margamfarms.vercel.app"],
       //methods: ["POST", "GET"],
       //credentials: true
 }
-));
+)); */
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 //Cookie Parser Middleware
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
-
-const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
+
+
+
+  app.get("*", function(req, res){
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'),function (err) {
           if(err) {
               res.status(500).send(err)
           }
       });
   })
-}
+
 
     app.get('/', (req, res) => {
       res.send('API is running....');
